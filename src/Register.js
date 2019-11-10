1f83a4028d546870a332a3b95716d48a65663334
PJMantoss <joelptoss@gmail.com>
2019-11-10 16:27:12 +0100

    Add logic to handleChange and handleSubmit

--- src/Register.js
+++ src/Register.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+
+const validateForm = errors => {
+    let valid = true;
+
+    Object.values(errors).forEach(
+        (val) => val.length > 0 && (valid = false)
+    )
+
+    return valid;
+}
+
+
 class Register extends React.Component {
     constructor(props){
         super(props);
@@ -15,6 +28,7 @@ class Register extends React.Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e){
@@ -23,10 +37,39 @@ class Register extends React.Component {
         let errors =  this.state.errors;
 
         switch(name){
-            case "fullName": 
+            case "fullName":
+                errors.fullName = value.length < 5 ? 'Full Name must be 5 characters long!' : '';
+                break; 
+
+            case 'email':
+                errors.email = validEmailRegex.test(value) ? '' : 'Email is not valid!';
+                break;
+
+            case 'password':
+                errors.password = value.length < 8 ? 'Password must be 8 characters!' : '';
+                break;
+
+            default:
+
+                break;
+        }
+
+        this.setState({errors, [name]: value});
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        if(validateForm(this.state.errors)){
+            console.info('Valid Form')
+        }else{
+            console.error('Invalid Form')
         }
     }
+
     render(){
+
+        const {errors} =  this.state;
+
         return (
             <div className="wrapper">
                 <div className="form-wrapper">
@@ -36,11 +79,12 @@ class Register extends React.Component {
                             <label htmlFor="fullName">Full Name</label>
                             <input 
                                 type="text" 
-                                name="name" 
+                                name="fullName" 
                                 onChange={this.handleChange}
                                 placeholder="Type your name"
                                 noValidate
                             />
+                            {errors.fullName.length > 0 && <span className='error'>{errors.fullName}</span>}
                         </div>
                         <div className="email">
                             <label htmlFor="email">E-Mail</label>
@@ -51,6 +95,7 @@ class Register extends React.Component {
                                 placeholder="Enter e-mail"
                                 noValidate
                             />
+                            {errors.email.length > 0 && <span className='error'>{errors.email}</span>}
                         </div>
                         <div className="password">
                             <label htmlFor="password">Password</label>
@@ -61,6 +106,7 @@ class Register extends React.Component {
                                 placeholder="Enter password"
                                 noValidate
                             />
+                            {errors.password.length > 0 && <span className='error'>{errors.password}</span>}
                         </div>
                         <div className="info">
                             <small>Password must be 8 characters long</small>
